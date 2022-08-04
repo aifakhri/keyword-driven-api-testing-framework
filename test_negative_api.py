d7ad1b67da96386104bfd1170bccedef09c8dcd6
fakhri <fakhri.azhar@proton.me>
2022-08-04 12:18:25 +0700

    New negative test case for books endpoints

---
+++ test_negative_api.py
@@ -0,0 +1,25 @@
+import pytest
+from endpoints.books import BookEndpoint
+
+
+@pytest.mark.parametrize("bookId", [
+    "someString",
+    "100"
+])
+def test_book_endpoint_path_attribute(bookId):
+    book = BookEndpoint()
+    book.getting_single_book(bookId=bookId)
+    
+    assert book.checking_status_code() == 404
+
+
+@pytest.mark.parametrize("type, limit", [
+    ("sport", 100),
+    ("sport", ""),
+    ("", 100),
+])
+def test_book_endpoint_query_param(type, limit):
+    book = BookEndpoint()
+    book.getting_multiple_books(type=type, limit=limit)
+    
+    assert book.checking_status_code() == 400
