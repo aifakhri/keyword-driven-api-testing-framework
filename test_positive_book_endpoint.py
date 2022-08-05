2822bdb51e84e6a678d218ee504317c4fa8a7e48
fakhri <fakhri.azhar@proton.me>
2022-08-05 08:59:01 +0700

    New Positive Test Case for Book Endpoint

---
+++ test_positive_book_endpoint.py
@@ -0,0 +1,29 @@
+import pytest
+from endpoints.books import BookEndpoint
+
+
+
+def test_multiple_books_endpoint():
+    books = BookEndpoint()
+    books.getting_multiple_books()
+    assert books.checking_status_code() == 201
+
+
+@pytest.mark.parametrize("bookId", [
+    1, 2, 3, 4, 5, 6
+])
+def test_single_books_endpoint(bookId):
+    books = BookEndpoint()
+    books.getting_single_book(bookId=bookId)
+    assert books.checking_status_code() == 200
+
+
+@pytest.mark.parametrize("type, limit", [
+    ("fiction", ""), ("non-fiction", ""), ("", 3), 
+    ("fiction", 2), ("non-ficton", 2)
+])
+def test_filtering_books(type, limit):
+    books = BookEndpoint()
+    books.getting_multiple_books(type=type, limit=limit)
+
+    assert books.checking_status_code() == 200
