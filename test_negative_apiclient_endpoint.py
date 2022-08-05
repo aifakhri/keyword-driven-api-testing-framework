cb10f3b05cfa8e7dafb41fae82a792fb612c009d
fakhri <fakhri.azhar@proton.me>
2022-08-05 08:55:37 +0700

    Negative Test Case for Book API

---
+++ test_negative_apiclient_endpoint.py
@@ -0,0 +1,15 @@
+import pytest
+from endpoints.apiClient import ApiClient
+
+
+
+@pytest.mark.parametrize("clientName, clientEmail", [
+    (12345, 12345), (12345, "JohnDoe"), ("JohnDoe", 12345)
+])
+def test_api_client_body_parameter(clientName, clientEmail):
+
+    api_client = ApiClient()
+    api_client.setup_api_client_detail(name=clientName, email=clientEmail)
+    api_client.register_api_client()
+
+    assert api_client == 400
