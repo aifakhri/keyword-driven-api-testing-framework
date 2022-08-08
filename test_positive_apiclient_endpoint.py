3038aabb361055f121366d209b0750e77e06b849
fakhri <fakhri.azhar@proton.me>
2022-08-08 21:55:53 +0700

    renaming file from test_validation to test_positive_order, also remove the apiclient test case and move it to a new file

---
+++ test_positive_apiclient_endpoint.py
@@ -0,0 +1,15 @@
+import pytest
+from endpoints.apiClient import ApiClient
+
+
+
+@pytest.mark.parametrize("custName, custEmail, expected_status_code", 
+    ["test", "test@something.com", 201],
+    ["test", "test@something.com", 409],
+)
+def test_api_client_endpoint(custName, custEmail, expected_status_code):
+    api_client = ApiClient()
+    api_client.setup_api_client_detail(name=custName, email=custEmail)
+    api_client.register_api_client()
+
+    assert api_client.checking_status_code() == expected_status_code
